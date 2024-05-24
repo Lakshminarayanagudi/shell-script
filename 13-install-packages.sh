ae9a41aa81c211279c8b9e5c4a2d3ae7fd35dada
lakshminarayanagudi <lakshminarayanagudi1@gmail.com>
2024-05-24 12:19:18 +0530

    added 2nd script

--- 13-install-packages.sh
+++ 13-install-packages.sh
@@ -34,10 +34,10 @@ fi # fi means reverse of if, indicating condition end
 
 for package in $@ 
 do
-    yum list installed $package $>> $LOGFILE #check installed or not
+    yum list installed $package &>> $LOGFILE #check installed or not
     if [$? -ne 0 ] # if not installed
     then 
-        yum install $package -y  $>> $LOGFILE # install the package
+        yum install $package -y  &>> $LOGFILE # install the package
         VALIDATE $? "installatin of $package" # validate
     else 
         echo -e "$package is already installed ... $Y SKIPPING $N"
