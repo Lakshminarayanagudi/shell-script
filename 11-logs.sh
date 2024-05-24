9496e390718380a1827b05124fd800dce798bfc1
lakshminarayanagudi <lakshminarayanagudi1@gmail.com>
2024-05-24 11:08:59 +0530

    added 2nd script

--- 11-logs.sh
+++ 11-logs.sh
@@ -4,7 +4,6 @@ ID=$(id -u)
 TIMESTAMP=$(date +%F-%H-%M-%S)
 R="\e[31m"
 G="\e[32m"
-Y="\e[33m"
 N="\e[0m"
 
 LOGFILE="/tmp/$0-$TIMESTAMP.log"
--- 13-install-packages.sh
+++ 13-install-packages.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
 ID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+
+if [ $? -ne 0 ]
+then
+    echo "$R ERROR:: Please run this script with root access $N"
+    exit 1 # you can give other than 0
+else
+    echo "You are root user"
 
 if [ $ID -ne 0 ]
 then
