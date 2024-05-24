90b22d97e0dfa41034a3f7505fe15c2e3b48bda7
lakshminarayanagudi <lakshminarayanagudi1@gmail.com>
2024-05-24 11:13:12 +0530

    added 2nd script

--- 13-install-packages.sh
+++ 13-install-packages.sh
@@ -11,13 +11,6 @@ then
     exit 1 # you can give other than 0
 else
     echo "You are root user"
-
-if [ $ID -ne 0 ]
-then
-    echo -e "$R ERROR:: Please run this script with root access $N"
-    exit 1 # you can give other than 0
-else
-    echo "You are root user"
 fi # fi means reverse of if, indicating condition end
 
-echo "All arguments passed: $@"
+echo "All arguments passed: $@
